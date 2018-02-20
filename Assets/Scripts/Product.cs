3784952d8101dab674fddfe6b5fa3c0424073cef
okg <okgpl@yahoo.pl>
2018-02-20 17:52:25 +0100

    Product Scripts: part 1.

---
+++ Assets/Scripts/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Cebula_Alert
+{
+	/// <summary>
+	/// Describes the product
+	/// </summary>
+	public class Product : MonoBehaviour
+	{
+		public double price;
+		public string productName;
+		public string amount; //used in inventory
+		public GameObject PriceHolder;
+		/// <summary>
+		/// Start this instance.
+		/// </summary>
+		void Start()
+		{
+			PriceHolder.GetComponent<TMPro.TextMeshPro> ().text = price.ToString ();
+		}
+	}
+}
