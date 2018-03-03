9041ba7f2bc31802cb54eaa8cad6273b0777e998
Dorthion <beleg.dorthion@gmail.com>
2018-03-03 23:45:39 +0100

    Commit bug

---
+++ Assets/Scripts/DollarButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollarButton : MonoBehaviour {
+
+    public ParticleSystem DollarParticle;
+
+	public void JakiDebug()
+    {
+        Debug.Log("Dawaj kase biedaku");
+        DollarParticle.Emit(1);
+    }
+}
