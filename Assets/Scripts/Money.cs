8d4672e9002cd81ad73b7131fb2ba368310364b2
okg <okgpl@yahoo.pl>
2018-03-05 13:16:31 +0100

    Game Mechanics + Shutdown

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -1,51 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace Cebula_Alert
+﻿using UnityEngine;
+/// <summary>
+/// Shows and services money balance
+/// </summary>
+public class Money : MonoBehaviour 
 {
 	/// <summary>
-	/// Shows and services money balance
+	/// Object which shows money balance (top of the screen)
 	/// </summary>
-	public class Money : MonoBehaviour 
+	public GameObject balanceViewer;
+	/// <summary>
+	/// Money balance
+	/// </summary>
+	private double moneyBalance = 99.99;
+	private string currencycode = "PLN";
+	public bool BuyMode = true;
+	/// <summary>
+	/// Updates balance viewer
+	/// </summary>
+	void Update () 
 	{
-		/// <summary>
-		/// Object which shows money balance (top of the screen)
-		/// </summary>
-		public GameObject balanceViewer;
-		/// <summary>
-		/// Money balance
-		/// </summary>
-		private double moneyBalance = 99.99;
-		private string currencycode = "PLN";
+		balanceViewer.GetComponent<TMPro.TextMeshPro>().text = (moneyBalance.ToString ()+ " " + currencycode);
+	}
+	/// <summary>
+	/// Add the specified value
+	/// </summary>
+	/// <param name="value">Amount of money to aff</param>
+	public void Add(double value)
+	{
+		moneyBalance += value;
+		Debug.Log ("Added " + value + "to balance");
+	}
 
-		/// <summary>
-		/// Updates balance viewer
-		/// </summary>
-		void Update () 
-		{
-			balanceViewer.GetComponent<TMPro.TextMeshPro>().text = (moneyBalance.ToString ()+ " " + currencycode);
-		}
-		/// <summary>
-		/// Add the specified value
-		/// </summary>
-		/// <param name="value">Amount of money to aff</param>
-		void Add(double value)
-		{
-			moneyBalance += value;
-		}
-
-		/// <summary>
-		/// Subtract the specified value
-		/// Returns 1 if balance is lower than amount, 0 otherwise
-		/// </summary>
-		/// <param name="value">Amount of money to subtract</param>
-		int Subtract (double value)
-		{
-			if (moneyBalance <= value) {
-				moneyBalance -= value;	
-				return 0;
-			} else
-				return 1;
-		}
+	/// <summary>
+	/// Subtract the specified value
+	/// Returns 1 if balance is lower than amount, 0 otherwise
+	/// </summary>
+	/// <param name="value">Amount of money to subtract</param>
+	public int Subtract (double value)
+	{
+		if (moneyBalance <= value) {
+			moneyBalance -= value;	
+			Debug.Log ("Subtracted " + value + "from balance");
+			return 0;
+		} else
+			return 1;
 	}
 }
--- Assets/Scripts/Product.cs
+++ Assets/Scripts/Product.cs
@@ -1,23 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace Cebula_Alert
+﻿using UnityEngine;
+
+/// <summary>
+/// Describes the product
+/// </summary>
+public class Product : MonoBehaviour
 {
-	/// <summary>
-	/// Describes the product
-	/// </summary>
-	public class Product : MonoBehaviour
+	public double buyPrice, sellPrice;
+	public string productName;
+	public uint amount; //for future, will be used by inventory
+	public GameObject PriceHolder; //object which shows a price
+	public bool isBuying; //player will buy item if it's true and sell if it's false
+
+	Product (string name = "AAA", uint amt = 1)
 	{
-		public double price;
-		public string productName;
-		public string amount; //used in inventory
-		public GameObject PriceHolder;
-		/// <summary>
-		/// Start this instance.
-		/// </summary>
-		void Start()
-		{
-			PriceHolder.GetComponent<TMPro.TextMeshPro> ().text = price.ToString ();
+		productName = name;
+		amount = amt;
+	}
+
+
+
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	void Update ()
+	{
+		isBuying = GameObject.Find ("MoneyBalance").GetComponent <Money> ().BuyMode;
+		if (isBuying)
+			PriceHolder.GetComponent<TMPro.TextMeshPro> ().text = buyPrice.ToString ();
+		else
+			PriceHolder.GetComponent<TMPro.TextMeshPro> ().text = sellPrice.ToString ();
+	}
+
+	public void onMouseDown ()
+	{
+		if (isBuying) {
+			if (GameObject.Find ("MoneyBalance").GetComponent <Money> ().Subtract (buyPrice) != 1) {
+				//GameObject.Find ("Inventory").GetComponent <Inventory> ().Add (gameObject.AddComponent( Product (productName, 1) != 1));
+				Debug.Log("Item " + productName + " bought");
+			}
+		} else {
+			//if (GameObject.Find ("Inventory").GetComponent <Inventory> ().Remove (gameObject.AddComponent( Product (productName, 1) != 1))) {
+				GameObject.Find ("MoneyBalance").GetComponent <Money> ().Add (sellPrice);
+				Debug.Log("Item " + productName +" sold");
+			//}
 		}
 	}
 }
