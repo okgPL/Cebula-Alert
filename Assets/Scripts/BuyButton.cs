39c134b616257bc729345a1acef6bf1323c121c3
okg <okgpl@yahoo.pl>
2018-03-06 16:56:28 +0100

    Gamemode buttons #1
    -Added a scrpit for BuyButton (still doesn't work)

---
+++ Assets/Scripts/BuyButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyButton : MonoBehaviour {
+
+    //public Button button;
+    public GameObject paskiBuy, paskiSell;
+
+    public void Click()
+    {
+        paskiBuy.SetActive(true);
+        paskiSell.SetActive(false);
+
+        Debug.Log("Gamemode set: Buy");
+        GameObject.Find("MoneyBalance").GetComponent < Money>().BuyMode = true;
+    }
+}
