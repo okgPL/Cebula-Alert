d6bf169813a49b21c8579eefe13f993244dc9ce4
okg <okgpl@yahoo.pl>
2018-03-07 11:26:48 +0100

    Game Mechanics #3: Sales

--- Assets/Scripts/Product.cs
+++ Assets/Scripts/Product.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 
 /// <summary>
 /// Describes the product
@@ -7,28 +8,31 @@ public class Product : MonoBehaviour
 {
     public double buyPrice, sellPrice;
     public string productName;
-    public uint amount; //for future, will be used by inventory
     public GameObject PriceHolder; //object which shows a price
     public bool isBuying; //player will buy item if it's true and sell if it's false
-
-    Product(string name = "AAA", uint amt = 1)
-    {
-        productName = name;
-        amount = amt;
-    }
-
-
+    private DateTime time;
+    public int saleDuration;
+    public int saleTrigger;
 
     /// <summary>
     /// Update this instance.
     /// </summary>
     void Update()
     {
+        time = DateTime.Now;
+        int minute = time.Minute;
+        int second = time.Second;
+        double sell;
+        if (minute % saleTrigger == 0 && saleDuration > second)
+            sell = sellPrice + sellPrice * 0.2;
+        else sell = sellPrice;
+
+
         isBuying = GameObject.Find("MoneyBalance").GetComponent<Money>().BuyMode;
         if (isBuying)
             PriceHolder.GetComponent<TMPro.TextMeshPro>().text = string.Format("{0:F2}", buyPrice);
         else
-            PriceHolder.GetComponent<TMPro.TextMeshPro>().text = string.Format("{0:F2}", sellPrice);
+            PriceHolder.GetComponent<TMPro.TextMeshPro>().text = string.Format("{0:F2}", sell);
     }
 
     public void ButtonClick()
@@ -36,17 +40,12 @@ public class Product : MonoBehaviour
         if (isBuying)
         {
             if (GameObject.Find("MoneyBalance").GetComponent<Money>().Subtract(buyPrice) != 1)
-            {
-                //GameObject.Find ("Inventory").GetComponent <Inventory> ().Add (gameObject.AddComponent( Product (productName, 1) != 1));
                 Debug.Log("Item " + productName + " bought");
-            }
         }
         else
         {
-            //if (GameObject.Find ("Inventory").GetComponent <Inventory> ().Remove (gameObject.AddComponent( Product (productName, 1) != 1))) {
             GameObject.Find("MoneyBalance").GetComponent<Money>().Add(sellPrice);
             Debug.Log("Item " + productName + " sold");
-            //}
         }
     }
 }
