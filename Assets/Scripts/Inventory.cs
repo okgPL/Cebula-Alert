55a4b04d0d57ada3c6ac38222e7757ae7f869552
okg <okgpl@yahoo.pl>
2018-03-10 16:58:32 +0100

    Inventory #2

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private int[] inventory = new int[2]; //2 because I wanted an array, please remember to increment that while adding a new item to the game
+
+    public void AddItem(int index, int amount)
+    {
+        inventory[index] += amount;
+    }
+
+    public bool RemoveItem(int index, int amount)
+    {
+        if (inventory[index]-amount < 0) return false;
+        else
+        {
+            inventory[index] -= amount;
+            return true;
+        }
+    }
+}
